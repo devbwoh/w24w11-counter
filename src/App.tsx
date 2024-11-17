861c0a0c0a584eaad9d2ba52f234bbd7b9672a91
BWOH <bwoh@outlook.kr>
2024-11-17 22:38:42 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        증가
+      </button>
+    </>
+  );
+}
+
+export default App;
+
+// import './App.css';
+// import { useState } from 'react';
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const plusOne = (prev: number) => prev + 1;
+
+//   return (
+//     <>
+//       <h1>{count}</h1>
+//       <button
+//         onClick={() => {
+//           setCount(plusOne);
+//           setCount(plusOne);
+//           setCount(plusOne);
+//           setCount(plusOne);
+//           setCount(plusOne);
+//         }}
+//       >
+//         증가
+//       </button>
+//     </>
+//   );
+// }
+
+// export default App;
